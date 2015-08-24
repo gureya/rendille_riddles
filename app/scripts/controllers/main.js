53a4a578c575e6042595633280601cb720f3f560
Gureya Daharewa Davis <gureya.daharewa@gmail.com>
2015-08-24 17:43:38 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo.question + " --- " + $scope.todo.answer);
+      $scope.todo.question = '';
+      $scope.todo.answer = '';
+    };
+
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
+  });
